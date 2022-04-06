556dc1d897236f830f4a408aabd7da2bfe68d4f9
user1 <csw999944@naver.com>
2022-04-06 11:42:07 +0900

    day4

--- src/server.js
+++ src/server.js
@@ -4,18 +4,14 @@ import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-
-
 const PORT = 4000;
+
 const app = express();
 const logger = morgan("dev");
 
 
-
-
-
-
-
+app.set("view engine","pug");
+app.set("views",process.cwd()+"/src/views");
 
 app.use(logger);
 app.use("/", globalRouter); 
